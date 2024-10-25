df671b68cf46725bc0dc5f7ebb0afbb1fcb03403
ThibeauK <123963805+ThibeauK@users.noreply.github.com>
2024-10-25 14:17:24 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    fetchComments();
+});
+
+function fetchComments() {
+    fetch('http://192.168.179.137:5000/get_comments')
+        .then(response => response.json())
+        .then(data => {
+            let commentsContainer = document.getElementById('posts-container');
+            commentsContainer.innerHTML = ''; // Clear the container
+            data.forEach(comment => {
+                let commentDiv = document.createElement('div');
+                commentDiv.className = 'comment';
+                commentDiv.innerHTML = `<strong>${comment.username}</strong>: ${comment.comment}`;
+                commentsContainer.appendChild(commentDiv);
+            });
+        });
+}
+
+function submitComment() {
+    let username = document.getElementById('username').value;
+    let comment = document.getElementById('comment').value;
+
+    if (username && comment) {
+        fetch('http://192.168.179.137:5000/add_comment', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username: username, comment: comment}),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.message);
+            fetchComments(); // Refresh comments after submitting
+        });
+    }
+}
